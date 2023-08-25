c34ba02d364c38a7517f175f37d25a65d0028127
ProfaIngrid <ingrid.batista@riobrancofac.edu.br>
2023-08-24 22:30:38 -0300

    aula01

---
+++ aula01/main.js
@@ -0,0 +1,26 @@
+//console.log eu uso para 
+//mostrar uma mensagem ao usuario
+console.log('Hello!');
+
+//alert exibe uma mensagem de alerta
+//ao usuário 
+//alert('Hello!');
+
+
+//para declarar variaveis no js
+//podemos usar "var", "let" ou "const"
+
+//SEMPRE que usar "const" para declarar
+//uma variavel, o valor dela NÃO
+//pode ser alterado!!!
+const pi = 3.14;
+
+//"let" é uma variável de escopo local
+//"var" é uma variável de escopo global
+
+var nome = "Ingrid";
+
+nome = "Maria";
+
+alert(nome);
+
