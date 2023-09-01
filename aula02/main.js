c0c047f57fb572bca0143987c675697334e97d86
ProfaIngrid <ingrid.pacheco2015@gmail.com>
2023-08-31 22:39:53 -0300

    aula 02

--- aula02/main.js
+++ aula02/main.js
@@ -1,6 +1,21 @@
+/*
 //crio uma variavel do tipo let 
 let nome = 'Maria';
 
+//exibo mensagem no console
 console.log(nome);
+//exibo caixa de alerta
+//alert(nome);
 
-alert(nome);
+nome = prompt("Qual o seu nome?");
+
+alert(`Ola ${nome}, boa noite`);
+*/
+
+let num1 = parseFloat(prompt("Digite um número:"));
+let num2 = parseFloat(prompt("Digite outro valor:"));
+
+let result = num1 + num2;
+
+alert(result);
+
